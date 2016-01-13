0e9ffdb4eb7d2acad4db02c8a203df42c132371c
MB Burch <marybethburch@gmail.com>
2016-01-12 19:45:58 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,61 @@
+var votes = {};
+
+const http = require('http');
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res){
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+const port = process.env.PORT || 3000;
+
+const server = http.createServer(app)
+                 .listen(port, function () {
+                    console.log('Listening on port ' + port + '.');
+                  });
+
+const socketIo = require('socket.io');
+const io = socketIo(server);
+
+io.on('connection', function (socket) {
+  console.log('A user has been connected.', io.engine.clientsCount);
+
+  io.sockets.emit('usersConnected', io.engine.clientsCount);
+
+  socket.emit('statusMessage', 'You have connected.');
+
+  socket.on('disconnect', function () {
+    console.log('A user has disconnected.', io.engine.clientsCount);
+    delete votes[socket.id];
+    socket.emit('voteCount', countVotes(votes));
+    io.sockets.emit('usersConnected', io.engine.clientsCount);
+  });
+
+  socket.on('message', function (channel, message) {
+    if (channel === 'voteCast') {
+      votes[socket.id] = message;
+      socket.emit('voteCount', countVotes(votes));
+    }
+  });
+
+});
+
+function countVotes (votes) {
+  var voteCount = {
+    A: 0,
+    B: 0,
+    C: 0,
+    D: 0
+  };
+  for (vote in votes) {
+    voteCount[votes[vote]]++
+  }
+  return voteCount;
+}
+
+module.exports = server;
+
